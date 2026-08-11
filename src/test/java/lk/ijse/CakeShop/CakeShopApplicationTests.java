7ff816d5f33da6cec2e67ee9b73f0bb1e26a6d42
sasinduKavishka-123 <sasindu.chemistry@gmail.com>
2026-08-11 19:38:58 +0530

    first commit

---
+++ src/test/java/lk/ijse/CakeShop/CakeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.CakeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
